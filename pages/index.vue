6ae9bffa7f0ab32eb8c1375b2955ead833329d40
Marco Sandri <marco.sandri@wearesim.it>
2023-04-03 12:39:07 +0200

    index

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,42 @@
 <template>
     <main>
-      Index
-      <!-- <Component :is="importDynamicComponent(component.ComponentName)" v-for="(component, index) in components " :key="index" :component="component"/> -->
+
+      <Component :is="importDynamicComponent(component.ComponentName)" v-for="(component, index) in components " :key="index" :component="component"/>
       
     </main>
 </template>
 
 <script setup>
-  // const route = useRoute();
-  // const { locale, locales } = useI18n();
-  // const { find } = useStrapi4();
+  const route = useRoute();
+  const { locale, locales } = useI18n();
+  const { find } = useStrapi4();
 
 
-  // const importDynamicComponent = (name) => {
-  //   let comp = resolveComponent(name)
-  //   return comp
-  // }
+  const importDynamicComponent = (name) => {
+    let comp = resolveComponent(name)
+    return comp
+  }
 
-  // const { data, pending, refresh, error } = await useAsyncData("system", () =>
-  //   find("system", {
-  //     populate: "deep",
-  //     locale: locale.value,
-  //   })
-  // );
+  const { data, pending, refresh, error } = await useAsyncData("system", () =>
+    find("system", {
+      populate: "deep",
+      locale: locale.value,
+    })
+  );
 
-  // const components = data.value.data.attributes.HomePage.data.attributes.Content || {}
-  // const localizations = data.value.data.attributes.HomePage.data.attributes.localizations.data || {}
+  const components = data.value.data.attributes.HomePage.data.attributes.Content || {}
+  const localizations = data.value.data.attributes.HomePage.data.attributes.localizations.data || {}
 
-  // const slugs = {}
+  const slugs = {}
 
-  // localizations.forEach((element) => {
-  //   let locale = element.attributes.locale;
-  //   slugs[locale] = { 'slug': element.attributes.Slug }
-  // });
+  localizations.forEach((element) => {
+    let locale = element.attributes.locale;
+    slugs[locale] = { 'slug': element.attributes.Slug }
+  });
 
-  // if(data.value) {
-  //   route.meta.nuxtI18n = slugs || {}
-  // }
+  if(data.value) {
+    route.meta.nuxtI18n = slugs || {}
+  }
 
 
   definePageMeta({
